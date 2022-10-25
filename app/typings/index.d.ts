939bd10333bca56be0dc13bde619589ff62574b9
Apoorv Mishra <apoorvmishra101092@gmail.com>
2022-10-25 21:40:47 +0530

    feat: add emoji-mart pkg and create Emoji component

--- app/typings/index.d.ts
+++ app/typings/index.d.ts
@@ -1,5 +1,7 @@
 declare module "autotrack/autotrack.js";
 
+declare module "emoji-mart";
+
 declare module "string-replace-to-array";
 
 declare module "sequelize-encrypted";
@@ -9,4 +11,10 @@ declare module "styled-components-breakpoint";
 declare module "*.png" {
   const value: any;
   export = value;
+}
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    "em-emoji": any;
+  }
 }
